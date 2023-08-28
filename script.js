df94866dc6b867ba774415013545af1a3a755ed7
mhorton <horton-michael@users.noreply.github.com>
2023-08-28 17:19:42 -0400

    adds function to display a question and start timer countdown on button click

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
 // DEPENDENCIES (DOM ELEMENTS)
-var timerEl = document.getElementById("timer");
-var mainEl = document.getElementsByClassName("main");
+var timerEl = $("#timer");
+var mainEl = $("main");
+var startButtonEl = $("#button");
 
 // DATA
+questionAnswerObj = {
+  "Arrays in JavaScript can be used to store ___.": [
+    "Numbers and strings",
+    "Other arrays",
+    "Booleans",
+    "All of the above",
+  ],
+  "Commonly used data types DO NOT include:": [
+    "Strings",
+    "Booleans",
+    "Alerts",
+    "Numbers",
+  ],
+};
+
+// variable to access indices in Object
+questionIndex = 0;
+// set of questions
+// answer choices
+// scores
 
 // FUNCTIONS
 // timer function
@@ -11,15 +32,28 @@ function countdown() {
   var timeInterval = setInterval(function () {
     if (timeLeft > 1) {
       timeLeft--;
-      timerEl.textContent = `Time: ${timeLeft}`;
+      timerEl.text(`Time: ${timeLeft}`);
     } else if (timeLeft <= 1) {
-      timerEl.textContent = "Time is up!";
+      timerEl.text("Time is up!");
       clearInterval(timeInterval);
     }
   }, 1000);
 }
 
+function displayQuestion() {
+  questionEl = $("#question");
+  currentQuestion = Object.keys(questionAnswerObj)[questionIndex];
+  questionEl.text(currentQuestion);
+}
+
 // USER INTERACTIONS
+startButtonEl.on("click", function () {
+  countdown();
+  questionIndex++;
+  if (questionIndex < Object.keys(questionAnswerObj).length) {
+    displayQuestion();
+  }
+  startButtonEl.hide();
+});
 
 // INITIALIZATIONS
-countdown();
